81a51247d02dbd1fa4b6a47e9ac666dc2c9f087b
byb4ek <157111926+byb4ek@users.noreply.github.com>
2024-07-20 13:37:31 +0300

    final

--- src/components/ToDoPresenter.ts
+++ src/components/ToDoPresenter.ts
@@ -2,18 +2,21 @@ import { IToDoModel } from '../types';
 import { IViewItem, IViewItemConstructor } from './Item';
 import { IForm, IFormConstructor } from './Form';
 import { IPage } from './Page';
+import { IPopup } from './Popup';
 
 export class ItemPresenter {
 	protected itemTemplate: HTMLTemplateElement;
 	protected formTemplate: HTMLTemplateElement;
 	protected todoForm: IForm;
 	protected todoEditForm: IForm;
+    protected handleSubmitEditForm: (data: {value: string}) => void;
 
 	constructor(
 		protected model: IToDoModel,
 		protected formConstructor: IFormConstructor,
 		protected viewPageContainer: IPage,
 		protected viewItemConstructor: IViewItemConstructor,
+		protected modal: IPopup,
         ) {
             this.itemTemplate = document.querySelector(
                 '#todo-item-template'
@@ -25,32 +28,53 @@ export class ItemPresenter {
 
         init() {
             this.todoForm = new this.formConstructor(this.formTemplate)
-            this.todoForm.setHandler(this.handleSubmitForm.bind(this));
+            this.todoForm.buttonText = 'Добавить';
+            this.todoForm.placeholder = 'Следующее дело';
             this.viewPageContainer.formContainer = this.todoForm.render();
-        }
+            this.todoEditForm = new this.formConstructor(this.formTemplate);
+            this.todoEditForm.buttonText = 'Изменить';
+            this.todoEditForm.placeholder = 'Новое название';
 
-        handleSubmitForm(data: string) {
-            this.model.addItem(data);
-            this.renderView();
+            this.model.on('changed', () => {
+                this.renderView();
+            })
+
+            this.todoForm.on('submit', this.handleSubmitForm.bind(this))
+            this.todoEditForm.on('submit', (data: {value: string}) => this.handleSubmitEditForm(data))
+            }
+
+        handleSubmitForm(data: {value: string}) {
+            this.model.addItem(data.value);
             this.todoForm.clearValue();
         }
-
-        handleCopyItem(item: IViewItem) {
+    
+        handleCopyItem(item: {id: string}) {
             const copyedItem = this.model.getItem(item.id)
             this.model.addItem(copyedItem.name);
-            this.renderView();
         }
     
-        handleDeleteItem(item: IViewItem) {
+        handleDeleteItem(item: {id: string}) {
             this.model.removeItem(item.id);
-            this.renderView();
         }    
 
+        handleEditItem(item: {id: string}) {
+            const editedItem = this.model.getItem(item.id)
+            this.todoEditForm.setValue(editedItem.name);
+            this.modal.content = this.todoEditForm.render();
+            this.handleSubmitEditForm = (data: {value: string}) => {
+                this.model.editItem(item.id, data.value);
+                this.todoEditForm.clearValue();
+                this.modal.close();   
+            }
+            this.modal.open();
+        }
+        
         renderView() {
             const itemList = this.model.items.map((item) => {
                 const todoItem = new this.viewItemConstructor(this.itemTemplate);
-    			todoItem.setCopyHandler(this.handleCopyItem.bind(this))
-    			todoItem.setDeleteHandler(this.handleDeleteItem.bind(this))
+    			todoItem.on('copy', this.handleCopyItem.bind(this))
+    			todoItem.on('delete', this.handleDeleteItem.bind(this))
+                todoItem.on('edit', this.handleEditItem.bind(this))
                 const itemElement = todoItem.render(item);
                 return itemElement;
             }).reverse();
